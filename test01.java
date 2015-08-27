8c7ed86fd8da18ea9a19f25cc85373eadb1324ed
koala998 <koala998@yahoo.com.tw>
2015-08-27 15:13:04 +0800

    no 2
    new breanch

--- test01.java
+++ test01.java
@@ -3,13 +3,7 @@ import java.util.*;
 public class test01{
 	public static void main(String[] args){
 		System.out.println("test01.java ****");
-		System.out.println("new line here");
-		System.out.println("new line here again");
-		System.out.println("new line here");
-		System.out.println("new line here again");
-		System.out.println("new line here");
-		System.out.println("new line here again");
-		System.out.println("new line here");
-		System.out.println("new line here again");
+		System.out.println("new breanch here");
+
 	}
 }
